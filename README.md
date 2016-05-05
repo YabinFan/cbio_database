171d6641b29626c62c5aaa9eb9bb7c26328c3cf1
shadowwalker <shadowwalker1117@hotmail.com>
2016-05-05 14:54:33 -0400

    add first tool

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cbio_database
+
+## Tools for data import
+
+1. parse .maf (TSV) file to CSV
+2. configure of database
+3. generate table creation sql
+4. import
---
+++ import_maf_YTK.py
@@ -0,0 +1,13 @@
+#!/bin/python
+#-*- coding: utf-8 -*-
+
+import csv, argparse
+
+def main():
+    """
+
+    :return:
+    """
+
+if __name__ == '__main__':
+    pass
