300daad9e42da35c09a5cf8c7c13220ec4e29eff
shadowwalker <shadowwalker1117@hotmail.com>
2016-05-05 16:06:04 -0400

    update tools

---
+++ gen_table_sql.py
@@ -0,0 +1,30 @@
+#!/bin/python
+#-*- coding: utf-8 -*-
+
+import argparse
+
+def main():
+    """
+
+    :return:
+    """
+    parser = argparse.ArgumentParser(description="A smart tool for generating CREATE TABLE sql.")
+    parser.add_argument("file", help=".csv filename for input")
+    parser.add_argument("-d", help="database name", default='yabin')
+    args = parser.parse_args()
+    inputfile = args.file
+
+    with open(inputfile, 'rb') as f:
+        headers = f.readline().split(',')
+
+    sql = "use %s;"%(args.d)+\
+          "\n-- change the table name below"+\
+          "\ncreate [table_name] ("+\
+          "\n-- modify the types of fields below"+\
+          "\t\n%s"%(' [type],\n\t'.join(headers))+\
+          "\n-- add constraints below"+\
+          "\n\n\n);"
+    print sql
+
+if __name__ == '__main__':
+    main()
